545deee40efda0a8c037ddcb4e5c06813ed33671
Yudann <yudannnnn80@gmail.com>
2024-11-04 11:59:53 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,71 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+// Dark / light mode
+
+const btn = document.getElementById("modeToggle");
+const btn2 = document.getElementById("modeToggle2");
+const themeIcons = document.querySelectorAll(".icon");
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme === "dark") {
+  setDarkMode();
+}
+
+btn.addEventListener("click", function () {
+  setTheme();
+});
+
+btn2.addEventListener("click", function () {
+  setTheme();
+});
+
+function setTheme() {
+  let currentTheme = document.body.getAttribute("theme");
+
+  if (currentTheme === "dark") {
+    setLightMode();
+  } else {
+    setDarkMode();
+  }
+}
+
+function setDarkMode() {
+  document.body.setAttribute("theme", "dark");
+  localStorage.setItem("theme", "dark");
+
+  themeIcons.forEach((icon) => {
+    icon.src = icon.getAttribute("src-dark");
+  });
+}
+
+function setLightMode() {
+  document.body.removeAttribute("theme");
+  localStorage.setItem("theme", "light");
+
+  themeIcons.forEach((icon) => {
+    icon.src = icon.getAttribute("src-light");
+  });
+}
+
+
+
+
+// // Hero Typed Effect
+// const typed = document.querySelector('.section__text__p2');
+
+// if(typed) {
+//   let typed_strings = typed.getAttribute('data-typed-items');
+//   typed_strings = typed_strings.split(',');
+//   new Typed('.typed',  {
+//     strings: typed_strings,
+//     loop: true,
+//     typeSpeed: 100,
+//     backSpeed: 50,
+//     backDelay: 2000,
+//   });
+// }
